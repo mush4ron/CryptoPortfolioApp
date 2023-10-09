7ff44ccb4f90b48ad278c79f3e0898aab9fa3f9e
producer23 <cherep633@gmail.com>
2023-10-09 19:26:40 +0300

    Finish Default Bottom Menu

--- app/src/main/java/com/rxs/cryptoportfolioapp/MainActivity.kt
+++ app/src/main/java/com/rxs/cryptoportfolioapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.rxs.cryptoportfolioapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.rxs.cryptoportfolioapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupBottomMenu()
+    }
+
+    private fun setupBottomMenu() {
+        supportFragmentManager.findFragmentById(R.id.fragmentContainerView)?.findNavController()
+            ?.let { binding.bottomNavigationView.setupWithNavController(it) }
     }
 }

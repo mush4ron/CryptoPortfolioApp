de71208bec7122327611021831e67645db646264
producer23 <cherep633@gmail.com>
2023-10-20 17:51:57 +0300

    Worked InvestDialog

---
+++ app/src/main/java/com/rxs/cryptoportfolioapp/presentation/portfolio/InvestDialogFragment.kt
@@ -0,0 +1,65 @@
+package com.rxs.cryptoportfolioapp.presentation.portfolio
+
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import androidx.fragment.app.DialogFragment
+import com.rxs.cryptoportfolioapp.databinding.FragmentInvestDialogBinding
+
+class InvestDialogFragment : DialogFragment() {
+
+    private lateinit var binding: FragmentInvestDialogBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentInvestDialogBinding.inflate(layoutInflater)
+        setupStyle()
+        setupButtons()
+
+        return binding.root
+    }
+
+    private fun setupButtons() {
+        binding.apply {
+            btnDialogInvestInvest.setOnClickListener {
+
+            }
+
+            btnDialogInvestBack.setOnClickListener {
+                dismiss()
+            }
+        }
+    }
+
+    private fun setupStyle() {
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
+
+        val marginInDp = 24
+        val marginInPixels = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            marginInDp.toFloat(),
+            requireContext().resources.displayMetrics
+        ).toInt()
+
+        val layoutParams = dialog?.window?.attributes
+        layoutParams?.gravity = Gravity.TOP
+        layoutParams?.y = marginInPixels
+        dialog?.window?.attributes = layoutParams
+    }
+
+    companion object {
+        const val TAG = "PurchaseConfirmationDialog"
+    }
+
+
+}
--- app/src/main/java/com/rxs/cryptoportfolioapp/presentation/portfolio/PortfolioFragment.kt
+++ app/src/main/java/com/rxs/cryptoportfolioapp/presentation/portfolio/PortfolioFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.rxs.cryptoportfolioapp.R
 import com.rxs.cryptoportfolioapp.databinding.FragmentPortfolioBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,9 +21,13 @@ class PortfolioFragment : Fragment() {
     ): View? {
         binding = FragmentPortfolioBinding.inflate(layoutInflater)
         setupView()
+
         return binding.root
     }
 
     private fun setupView() {
+        binding.btnFragmentPortfolioInvest.setOnClickListener {
+            Navigation.findNavController(binding.root).navigate(R.id.action_portfolioFragment_to_investDialogFragment)
+        }
     }
 }
